86b18bca7d998cf8f0e1796f1b86c3cd7081bb95
Rebecca Mooney <astrakiseki@gmail.com>
2016-04-10 07:16:36 -0700

    Adjustments in regards to the front end

--- PropertyManager.Api/PropertyManager.Api/Infrastructure/PropertyManagerAuthorizationServerProvider.cs
+++ PropertyManager.Api/PropertyManager.Api/Infrastructure/PropertyManagerAuthorizationServerProvider.cs
@@ -33,7 +33,7 @@ namespace PropertyManager.Api.Infrastructure
                 else
                 {
                     var token = new ClaimsIdentity(context.Options.AuthenticationType);
-                    token.AddClaim(new Claim("sub", context.UserName));
+                    token.AddClaim(new Claim(ClaimTypes.Name, context.UserName));
                     token.AddClaim(new Claim("role", "user"));
 
                     context.Validated(token);
